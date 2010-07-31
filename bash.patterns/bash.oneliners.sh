12888dbe453ad2a10a852171cc616cf3db558ed1
Richard Bronosky <richard.bronosky@cmgdigital.com>
2010-07-30 23:42:19 -0400

    a few more oneliners.

--- bash.patterns/bash.oneliners.sh
+++ bash.patterns/bash.oneliners.sh
@@ -1,3 +1,6 @@
+# Keep these generic and SAFE!
+# Anyone should be safe to run these anywhere anytime to see what the pattern does.
+
 # mimic basename
 echo ${HOME##*/}
 
@@ -9,6 +12,15 @@ read -sn 1 -p "Continue? [Y/n] " response; echo
 
 # prompt for a string/int
 read -p "username: " user;
+
+# capture pid of last command ($!; is a problem, needs a space)
+sleep 1& PID=$! ; ps $PID
+
+# create a pid file init script style
+echo $! > ${0%.*}.pid
+
+# do something for n seconds
+c=0; sleep 1 & p=$! ; while true; do ps $p >/dev/null && echo $((c++)) || break; done
 
 # Timestamp for a file name
 date +%s
