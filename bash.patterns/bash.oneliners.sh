e8e94e3788f7c4c0053824c2bdbea26abd634dcb
Richard Bronosky <richard.bronosky@cmgdigital.com>
2010-07-30 23:15:22 -0400

    a few more oneliners.

--- bash.patterns/bash.oneliners.sh
+++ bash.patterns/bash.oneliners.sh
@@ -4,8 +4,17 @@ echo ${HOME##*/}
 # mimic dirname
 echo ${HOME%/*}
 
+# prompt for a bool/char
+read -sn 1 -p "Continue? [Y/n] " response; echo
+
+# prompt for a string/int
+read -p "username: " user;
+
 # Timestamp for a file name
 date +%s
 
 # a readable one
 date +%Y%m%d%H%M%S
+
+# DiG format as a clean single line for batch processing
+dig +nocmd www.bronosky.com +noall +answer
